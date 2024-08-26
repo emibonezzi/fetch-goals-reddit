18f8ce35bfc95ccb278362c1d8fb724c805facfa
emibonezzi <emibonezzi@gmail.com>
2024-08-26 08:54:51 -0400

    Add truzgoals user_id

--- handlers/uploadToThreads.js
+++ handlers/uploadToThreads.js
@@ -3,8 +3,16 @@ const axios = require("axios");
 module.exports = async (title, url) => {
   try {
     // create media container
+    console.log(
+      "video_url",
+      url,
+      "text:",
+      title,
+      "access_token",
+      process.env.THREADS_ACCESS_TOKEN
+    );
     const mediaContainer = await axios.post(
-      "https://graph.threads.net/v1.0/me/threads",
+      "https://graph.threads.net/v1.0/8063816050370756/threads",
       {
         media_type: "VIDEO",
         video_url: url,
@@ -12,12 +20,22 @@ module.exports = async (title, url) => {
         access_token: process.env.THREADS_ACCESS_TOKEN, // change token in 60 days from august 25 2024
       }
     );
+    const mediaID = mediaContainer.data.id;
+    console.log(
+      "***** MEDIA ID *****",
+      mediaID,
+      "**** FULL MEDIA CONTAINER ****",
+      mediaContainer
+    );
     // post
-    await axios.post("https://graph.threads.net/v1.0/me/threads_publish", {
-      creation_id: mediaContainer.data.id,
-      access_token: process.env.THREADS_ACCESS_TOKEN,
-    });
+    await axios.post(
+      "https://graph.threads.net/v1.0/8063816050370756/threads_publish",
+      {
+        creation_id: mediaID,
+        access_token: process.env.THREADS_ACCESS_TOKEN,
+      }
+    );
   } catch (err) {
-    console.log("Error in uploading to Threads...moving on");
+    console.log("Error in uploading to Threads...moving on", err.message);
   }
 };
